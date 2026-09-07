53adf5bce0e5b949e99eea1e832f7300005cea8f
ky0nq <141110673+ky0nq@users.noreply.github.com>
2026-09-08 08:41:33 +0900

    Refactor warmup_frames declaration in env_cfg

--- UVM/OV7670_VGA/scenario2/tb/env/env_cfg.sv
+++ UVM/OV7670_VGA/scenario2/tb/env/env_cfg.sv
@@ -6,7 +6,7 @@
 class env_cfg extends uvm_object;
   `uvm_object_utils(env_cfg)
   bit scale2x       = 1'b0;
-  int warmup_frames = 2;   // 프레임버퍼 채워질 때까지 무시할 VGA 프레임 수
+  int warmup_frames = 2;   
   function new(string name="env_cfg"); super.new(name); endfunction
 endclass
 `endif
