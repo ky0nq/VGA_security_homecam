175e9f332735451a830bcb35f986f6afed696796
ky0nq <141110673+ky0nq@users.noreply.github.com>
2026-09-08 08:44:29 +0900

    Update vga_probe_if.sv

--- UVM/OV7670_VGA/scenario2/tb/intf/vga_probe_if.sv
+++ UVM/OV7670_VGA/scenario2/tb/intf/vga_probe_if.sv
@@ -1,12 +1,13 @@
 `timescale 1ns/1ps
 //======================================================================
-//  vga_probe_if : top_OV7670_VGA 내부 신호 관측 (bind 로 삽입)
+//  vga_probe_if : monitors internal signals of top_OV7670_VGA
+//                 (inserted using bind)
 //======================================================================
 interface vga_probe_if (
   input logic       clk,
   input logic       reset,
   input logic       de,
-  input logic       pix_tick,     // VGA_Decoder 내부 pclk(÷4) 펄스
+  input logic       pix_tick,     // pclk (÷4) pulse inside VGA_Decoder
   input logic [9:0] x_pixel,
   input logic [9:0] y_pixel,
   input logic       h_sync,
@@ -15,7 +16,7 @@ interface vga_probe_if (
   input logic [3:0] green,
   input logic [3:0] blue
 );
-  // race 방지용 샘플링 클로킹 블록
+  // Sampling clocking block to prevent race conditions
   clocking cb @(posedge clk);
     input de, pix_tick, x_pixel, y_pixel, h_sync, v_sync, red, green, blue;
   endclocking
