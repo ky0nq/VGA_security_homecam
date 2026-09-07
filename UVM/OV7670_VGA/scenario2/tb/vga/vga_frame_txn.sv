211d0545d763d537b35c60c34725ab8e45963711
ky0nq <141110673+ky0nq@users.noreply.github.com>
2026-09-08 08:49:38 +0900

    Update vga_frame_txn.sv

--- UVM/OV7670_VGA/scenario2/tb/vga/vga_frame_txn.sv
+++ UVM/OV7670_VGA/scenario2/tb/vga/vga_frame_txn.sv
@@ -1,7 +1,7 @@
 `ifndef VGA_FRAME_TXN_SV
 `define VGA_FRAME_TXN_SV
 //======================================================================
-//  vga_frame_txn : 관측된 VGA 한 프레임 (active 영역)
+//  vga_frame_txn : represents one observed VGA frame (active region)
 //======================================================================
 class vga_frame_txn extends uvm_sequence_item;
   int       HD = `VGA_HACT;
@@ -10,7 +10,9 @@ class vga_frame_txn extends uvm_sequence_item;
   bit [3:0] g [];
   bit [3:0] b [];
   bit       seen [];
+
   `uvm_object_utils(vga_frame_txn)
+
   function new(string name="vga_frame_txn");
     super.new(name);
     r    = new[HD*VD];
